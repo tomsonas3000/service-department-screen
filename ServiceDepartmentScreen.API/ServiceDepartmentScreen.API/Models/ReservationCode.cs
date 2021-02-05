5b6152f5db88ed3b1367794f86855db4a59635f5
tomsonas3000 <tomasjorudas23@gmail.com>
2021-02-05 11:23:50 +0200

    Add specialist and reservation code models

---
+++ ServiceDepartmentScreen.API/ServiceDepartmentScreen.API/Models/ReservationCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceDepartmentScreen.API.Models
+{
+    public class ReservationCode
+    {
+        public int ReservationCodeId { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public bool HasBegun { get; set; }
+        public bool IsCancelled { get; set; }
+        public bool HasEnded { get; set; }
+        public Specialist Specialist { get; set; }
+    }
+}
