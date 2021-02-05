97b84934035120190485a17021bcad417b20d25c
tomsonas3000 <tomasjorudas23@gmail.com>
2021-02-05 12:27:50 +0200

    Add seed data migration

---
+++ ServiceDepartmentScreen.API/ServiceDepartmentScreen.API/Migrations/20210205102553_SeedData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServiceDepartmentScreen.API.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ReservationCodes",
+                columns: new[] { "ReservationCodeId", "HasBegun", "HasEnded", "IsCancelled", "ReservationDate", "SpecialistId" },
+                values: new object[,]
+                {
+                    { 1, false, false, true, new DateTime(2021, 2, 5, 22, 15, 25, 0, DateTimeKind.Unspecified), null },
+                    { 2, false, false, false, new DateTime(2021, 2, 5, 16, 15, 25, 0, DateTimeKind.Unspecified), null },
+                    { 3, false, false, false, new DateTime(2021, 2, 5, 18, 15, 25, 0, DateTimeKind.Unspecified), null },
+                    { 4, false, false, false, new DateTime(2021, 2, 5, 12, 15, 25, 0, DateTimeKind.Unspecified), null },
+                    { 5, false, true, true, new DateTime(2021, 2, 5, 10, 15, 25, 0, DateTimeKind.Unspecified), null },
+                    { 6, false, false, true, new DateTime(2021, 2, 5, 22, 15, 25, 0, DateTimeKind.Unspecified), null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Specialists",
+                columns: new[] { "SpecialistId", "FirstName", "LastName", "Password", "Username" },
+                values: new object[,]
+                {
+                    { 1, "Specialist1", "Specialist1", "123456", "spec1" },
+                    { 2, "Specialist2", "Specialist2", "123456", "spec2" },
+                    { 3, "Specialist3", "Specialist3", "123456", "spec3" },
+                    { 4, "Specialist4", "Specialist4", "123456", "spec4" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ReservationCodes",
+                keyColumn: "ReservationCodeId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ReservationCodes",
+                keyColumn: "ReservationCodeId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "ReservationCodes",
+                keyColumn: "ReservationCodeId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "ReservationCodes",
+                keyColumn: "ReservationCodeId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ReservationCodes",
+                keyColumn: "ReservationCodeId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "ReservationCodes",
+                keyColumn: "ReservationCodeId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Specialists",
+                keyColumn: "SpecialistId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Specialists",
+                keyColumn: "SpecialistId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Specialists",
+                keyColumn: "SpecialistId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Specialists",
+                keyColumn: "SpecialistId",
+                keyValue: 4);
+        }
+    }
+}
--- ServiceDepartmentScreen.API/ServiceDepartmentScreen.API/Models/AppDbContext.cs
+++ ServiceDepartmentScreen.API/ServiceDepartmentScreen.API/Models/AppDbContext.cs
@@ -15,5 +15,92 @@ namespace ServiceDepartmentScreen.API.Models
         }
         public DbSet<ReservationCode> ReservationCodes { get; set; }
         public DbSet<Specialist> Specialists { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Specialist>().HasData(new Specialist
+            { 
+                SpecialistId = 1,
+                FirstName = "Specialist1",
+                LastName = "Specialist1", 
+                Password = "123456", 
+                Username = "spec1" 
+            });
+            modelBuilder.Entity<Specialist>().HasData(new Specialist
+            {
+                SpecialistId = 2,
+                FirstName = "Specialist2",
+                LastName = "Specialist2",
+                Password = "123456",
+                Username = "spec2"
+            });
+            modelBuilder.Entity<Specialist>().HasData(new Specialist
+            {
+                SpecialistId = 3,
+                FirstName = "Specialist3",
+                LastName = "Specialist3",
+                Password = "123456",
+                Username = "spec3"
+            });
+            modelBuilder.Entity<Specialist>().HasData(new Specialist
+            {
+                SpecialistId = 4,
+                FirstName = "Specialist4",
+                LastName = "Specialist4",
+                Password = "123456",
+                Username = "spec4"
+            });
+
+            modelBuilder.Entity<ReservationCode>().HasData(new ReservationCode
+            {
+                ReservationCodeId = 1,
+                ReservationDate = new DateTime(2021,02,05,22,15,25),
+                HasBegun = false,
+                HasEnded = false,
+                IsCancelled = true
+            });
+            modelBuilder.Entity<ReservationCode>().HasData(new ReservationCode
+            {
+                ReservationCodeId = 2,
+                ReservationDate = new DateTime(2021, 02, 05, 16, 15, 25),
+                HasBegun = false,
+                HasEnded = false,
+                IsCancelled = false
+            });
+            modelBuilder.Entity<ReservationCode>().HasData(new ReservationCode
+            {
+                ReservationCodeId = 3,
+                ReservationDate = new DateTime(2021, 02, 05, 18, 15, 25),
+                HasBegun = false,
+                HasEnded = false,
+                IsCancelled = false
+            });
+            modelBuilder.Entity<ReservationCode>().HasData(new ReservationCode
+            {
+                ReservationCodeId = 4,
+                ReservationDate = new DateTime(2021, 02, 05, 12, 15, 25),
+                HasBegun = false,
+                HasEnded = false,
+                IsCancelled = false
+            });
+            modelBuilder.Entity<ReservationCode>().HasData(new ReservationCode
+            {
+                ReservationCodeId = 5,
+                ReservationDate = new DateTime(2021, 02, 05, 10, 15, 25),
+                HasBegun = false,
+                HasEnded = true,
+                IsCancelled = true
+            });
+            modelBuilder.Entity<ReservationCode>().HasData(new ReservationCode
+            {
+                ReservationCodeId = 6,
+                ReservationDate = new DateTime(2021, 02, 05, 22, 15, 25),
+                HasBegun = false,
+                HasEnded = false,
+                IsCancelled = true
+            });
+        }
     }
 }
