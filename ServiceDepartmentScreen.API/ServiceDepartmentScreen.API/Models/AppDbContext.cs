7d52b7ef0eb1077a8e741a96a2e85be557d39adf
tomsonas3000 <tomasjorudas23@gmail.com>
2021-02-05 15:45:23 +0200

    Move models to separate project

--- ServiceDepartmentScreen.API/ServiceDepartmentScreen.API/Models/AppDbContext.cs
+++ ServiceDepartmentScreen.API/ServiceDepartmentScreen.API/Models/AppDbContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ServiceDepartmentScreen.Shared;
 
 namespace ServiceDepartmentScreen.API.Models
 {
