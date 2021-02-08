ebb6a53e43f95a7552d60997fcaf45769fd7cf36
tomsonas3000 <tomasjorudas23@gmail.com>
2021-02-08 14:08:54 +0200

    Add specialist service

--- ServiceDepartmentScreen.API/ServiceDepartmentScreen.WebApp/Pages/SpecialistPages/SpecialistOverview.cs
+++ ServiceDepartmentScreen.API/ServiceDepartmentScreen.WebApp/Pages/SpecialistPages/SpecialistOverview.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 using ServiceDepartmentScreen.Shared;
+using ServiceDepartmentScreen.WebApp.Services;
 
 namespace ServiceDepartmentScreen.WebApp.Pages.SpecialistPages
 {
-    public class SpecialistOverview
+    public partial class SpecialistOverview
     {
+        [Inject]
+        public ISpecialistService SpecialistService { get; set; }
+        [Inject]
+        public IReservationCodeService ReservationCodeService { get; set; }
         public Specialist Specialist { get; set; }   
+        public IEnumerable<ReservationCode> ReservationCodes { get; set; }
+
     }
 }
